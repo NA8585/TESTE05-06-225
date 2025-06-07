09da3b4e25d5b803afcb8f1acc21bc7aa90e420f
Nasser <doctor_reda@hotmail.com>
2025-06-07 04:31:01 -0300

    Calculate average fuel from last 3 laps

--- backend/Models/TelemetryCalculationsOverlay.cs
+++ backend/Models/TelemetryCalculationsOverlay.cs
@@ -33,11 +33,10 @@ namespace SuperBackendNR85IA.Calculations
 
             model.LapsRemaining = (int)TelemetryCalculations.GetFuelLapsLeft(model.FuelLevel, model.ConsumoVoltaAtual);
 
-            float lapsEfetivos = model.Lap + model.LapDistPct;
-            model.ConsumoMedio = (lapsEfetivos > 0 && model.FuelUsedTotal > 0)
-                ? model.FuelUsedTotal / lapsEfetivos
-                : 0f;
-            model.VoltasRestantesMedio = model.ConsumoMedio > 0 ? model.FuelLevel / model.ConsumoMedio : 0;
+            // Consumo médio calculado no serviço a partir das três últimas voltas
+            model.VoltasRestantesMedio = model.ConsumoMedio > 0
+                ? model.FuelLevel / model.ConsumoMedio
+                : 0;
             model.NecessarioFim = (float)TelemetryCalculations.GetFuelForTargetLaps(
                 model.LapsRemainingRace, model.ConsumoMedio);
 
